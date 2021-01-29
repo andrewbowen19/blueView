ec7cc2a1992f58a454dc41e2a1984e7813de5628
Andrew Bowen <atbowen@gmail.com>
2021-01-28 21:28:54 -0500

    No SQL db in repo

--- scheduler.py
+++ scheduler.py
@@ -7,8 +7,7 @@ https://www.postgresqltutorial.com/postgresql-python/update/
 import os
 import psycopg2
 # Connection url -- env variable in production
-dns = 'postgres://tssziceensuyju:df46ddbe5911723f5b7c920bcda753f4cc9fa50215ae9b4b92129fe354a3e0da@ec2-3-232-92-90.compute-1.amazonaws.com:5432/d6ij7g4596l6t4'
-conn = psycopg2.connect(dns)
+# dns = 
 cur = conn.cursor()
 
 value1=10
